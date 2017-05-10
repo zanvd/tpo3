03e244dc58ee8f87b909fbbe2442c29cfb8b62cc
Matevz Cesar <mc8873@student.uni-lj.si>
2017-05-10 18:24:26 +0200

    quick fix pt 5

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -22,7 +22,7 @@
           <h2>Registracija uspešna!</h2>
           <p>Na vaš e-poštni naslov smo vam poslali povezavo za aktivacijo vašega računa. Če sporočila niste prejeli, ga pošljite ponovno s spodnjim obrazcem. </p>
           <div class="col-md-3">
-          <input class="form-control" @if(!empty($email)) value ={{$email}} @endif type="email" placeholder="Vnestie e-naslov..." name="email" id="email" required>
+          <input class="form-control" @if(!empty($email)) value ="{{$email}}" @endif type="email" placeholder="Vnestie e-naslov..." name="email" id="email" required>
           <input type="hidden" name="_method" value="put"> 
           </div>
           <button class="btn btn-primary" type="submit">Pošlji</button>
