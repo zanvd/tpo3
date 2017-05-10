aea97bf1528b87aafc6695e4145455ae25461d86
Matevz Cesar <mc8873@student.uni-lj.si>
2017-05-10 18:02:07 +0200

    quick fix pt 4

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -6,12 +6,24 @@
 
 @section('content')
 <div class="container">
+		<div class="row">
+			@if( $status = Session::get('status'))
+				<div class="alert alert-success" role="alert">{{ $status }}</div>
+			@endif
+			@if (count($errors))
+				@foreach($errors->all() as $error)
+					<div class="alert alert-danger">{{ $error }}</div>
+				@endforeach					
+			@endif
+		</div>
         <div class="jumbotron">
-        <form class="article-comment" id="registrationForm" method="POST" name="registrationForm" action="/registracija">
+
+        <form class="article-comment" id="registrationForm" method="POST" name="registrationForm" action="/verifikacija">
           <h2>Registracija uspešna!</h2>
           <p>Na vaš e-poštni naslov smo vam poslali povezavo za aktivacijo vašega računa. Če sporočila niste prejeli, ga pošljite ponovno s spodnjim obrazcem. </p>
           <div class="col-md-3">
-          <input class="form-control" @if(!empty($email)) value ={{$email}} @endif type="email" placeholder="Vnestie e-naslov..." name="email" id="email" required> 
+          <input class="form-control" @if(!empty($email)) value ={{$email}} @endif type="email" placeholder="Vnestie e-naslov..." name="email" id="email" required>
+          <input type="hidden" name="_method" value="put"> 
           </div>
           <button class="btn btn-primary" type="submit">Pošlji</button>
         </form>
