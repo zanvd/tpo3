fab53954a5b861f8cab87e840d7e4e90cdf2eda4
Žan V. Dragan <zan10d@gmail.com>
2017-06-03 01:37:17 +0200

    Finished with forgotten password.

--- app/Controllers/Auth/ResetPasswordController.php
+++ app/Controllers/Auth/ResetPasswordController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
+use App\Models\ResetLink;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
 
@@ -10,14 +13,7 @@ class ResetPasswordController extends Controller {
 	use ResetsPasswords;
 
 	/**
-	 * Where to redirect users after resetting their password.
-	 *
-	 * @var string
-	 */
-	protected $redirectTo = '/';
-
-	/**
-	 * Create a new controller instance.
+	 * Create a new reset password controller instance.
 	 *
 	 */
 	public function __construct () {
@@ -32,7 +28,14 @@ class ResetPasswordController extends Controller {
 	 *
 	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
-	public function index ($email = null, $token = null) {
+	public function edit ($email = null, $token = null) {
+		// Check if there is data missing and ask for another request.
+		if (is_null($email) || is_null($token))
+			return view('forgotten')->withErrors([
+				'message'	=> 'Prišlo je do napake pri povezavi do obrazca.'
+								. ' Prosimo, pošljite ponovno zahtevo.'
+				]);
+
 		return view('resetPassword')->with([
 			'token' => $token,
 			'email' => $email
@@ -44,22 +47,68 @@ class ResetPasswordController extends Controller {
 	 *
 	 * @param Request $request
 	 *
-	 * @return \Illuminate\Http\RedirectResponse
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\View\View
 	 */
-	public function store (Request $request) {
-		return $this->reset($request);
-	}
-
-	/**
-	 * Get the password reset validation rules.
-	 *
-	 * @return array
-	 */
-	protected function rules() {
-		return [
+	public function update (Request $request) {
+		// Validate request.
+		$this->validate($request, [
 			'token'		=> 'required',
 			'email'		=> 'required|email',
-			'password'	=> 'required|confirmed|min:8|max:64',
-		];
+			'password'	=> 'required|confirmed|min:8|max:64'
+		]);
+
+		// Check if a user with this email exists.
+		$user = User::where('email', $request['email'])->first();
+		if (!isset($user))
+			return view('forgotten')->withErrors([
+				'message'	=> 'Uporabniški račun ni bil najden.'
+								. ' Prosimo, pošljite ponovno zahtevo.'
+			]);
+
+		// Check if there is actually a reset token.
+		$resetLink = ResetLink::where('email', $request['email'])->first();
+		if (!isset($resetLink))
+			return view('forgotten')->withErrors([
+				'message'	=> 'Žeton za ponastavitev ni bil najden.'
+								. ' Prosimo, pošljite ponovno zahtevo.'
+			]);
+
+		// Check if token is still valid.
+		if (Carbon::now()->subHour() > $resetLink->created_at) {
+			// Change token and resend it.
+			$forgotten = new ForgotPasswordController();
+			return $forgotten->update($request);
+		}
+
+		// Verify token.
+		if ($resetLink->token !== $request['token']) {
+			return view('forgotten')->withErrors([
+				'message'	=> 'Žeton za ponastavitev ni pravilen.'
+								. ' Prosimo, pošljite ponovno zahtevo.'
+			]);
+		}
+
+		try {
+			// Update password
+			$user->update([
+				'password'	=> bcrypt($request['password'])
+			]);
+
+			// Remove reset token.
+			$resetLink->delete();
+
+		} catch (\Exception $e) {
+			error_log(print_r('Error resetting password: ' . $e, true));
+
+			return back()->with([
+				'message'	=> 'Napaka pri ponstavljanju gesla.'
+								. ' Prosimo, poskusite ponovno'
+				]);
+		}
+
+
+		return redirect('/prijava')->with([
+			'status'	=> 'Geslo uspešno ponastavljeno.'
+		]);
 	}
 }
